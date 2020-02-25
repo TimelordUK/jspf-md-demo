6f3499efb639207f5afc4127318ef82da2dba51f
Stephen <sjames8888@gmail.com>
2020-02-25 21:00:34 +0000

    start market data demo using quickfix data dictionary with custom fields

---
+++ src/app/app.ts
@@ -0,0 +1,25 @@
+import { MDClient } from './md-client'
+import { MDServer } from './md-server'
+import { IJsFixConfig, initiator, acceptor } from 'jspurefix'
+import { Launcher } from './launcher'
+
+class AppLauncher extends Launcher {
+  public constructor () {
+    super(
+      './../../data/session/test-initiator.json',
+      './../../data/session/test-acceptor.json')
+  }
+
+  protected getAcceptor (config: IJsFixConfig): Promise<any> {
+    return acceptor(config, c => new MDServer(c))
+  }
+
+  protected getInitiator (config: IJsFixConfig): Promise<any> {
+    return initiator(config, c => new MDClient(c))
+  }
+}
+
+const l = new AppLauncher()
+l.run().then(() => {
+  console.log('finished.')
+})
