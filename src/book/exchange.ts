6b812828c9d6728a62272c681e55b539d4fc7937
Stephen <sjames8888@gmail.com>
2020-03-04 21:03:39 +0000

    start a simple matcher to allow buy and sell orders to cross

--- src/book/exchange.ts
+++ src/book/exchange.ts
@@ -1,9 +1,19 @@
 import { INewOrderSingle } from '../types'
 import { MatchingEngine } from './matching-engine'
+import { Order, Side } from './order'
+import { Side as FixSide } from '../types/enum'
 
 export class Exchange {
   private matcher: MatchingEngine = new MatchingEngine()
   public newOrder (nos: INewOrderSingle): void {
   // receive a mew order add to book
+    const o = new Order(1, nos.Instrument.Symbol,
+      nos.Side === FixSide.Buy ? Side.Buy : Side.Sell,
+      nos.Price,
+      nos.OrderQtyData.OrderQty, 0,
+      nos.ClOrdID,
+      nos.OrderQtyData.OrderQty,
+      nos.TradeDate)
+    this.matcher.newOrder(o)
   }
 }
--- src/test/matching.test.ts
+++ src/test/matching.test.ts
@@ -1,5 +1,6 @@
-import { Order, Side } from '../book'
+import { Exchange, Order, Side } from '../book'
 import { MatchingEngine } from '../book/matching-engine'
+import { INewOrderSingle, OrdType, OrderCapacity, TimeInForce, Side as FixSide, CustOrderCapacity } from '../types'
 
 test('add new order to matcher', () => {
   const matcher = new MatchingEngine()
@@ -12,4 +13,26 @@ test('add new order to matcher', () => {
   expect(book.buys.find(o1.price)).toBeTruthy()
   expect(book.buys.find(o1.price + 0.1)).toEqual(null)
   expect(book.buys.find(o1.price).quantity()).toEqual(o1.quantity)
+})
+
+test('enter NewOrderSingle to exchange', () => {
+  const o1 = {
+    ClOrdID: 'Order184946',
+    Account: '2F8QNWXP2',
+    Instrument: {
+      Symbol: 'COSP:BTC/USD'
+    },
+    Side: FixSide.Buy,
+    TransactTime: new Date('2019-04-02T14:22:27.000Z'),
+    OrderQtyData: {
+      OrderQty: 5
+    },
+    OrdType: OrdType.Limit,
+    Price: 7300,
+    TimeInForce: TimeInForce.Day,
+    OrderCapacity: OrderCapacity.Agency,
+    CustOrderCapacity: CustOrderCapacity.MemberTradingForTheirOwnAccount
+  } as INewOrderSingle
+  const exchange = new Exchange()
+  exchange.newOrder(o1)
 })
