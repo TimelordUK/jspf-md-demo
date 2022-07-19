7cc1511251c3232ffb8e281dde397ac6b01e9c10
Stephen James <sjames8888@gmail.com>
2022-07-19 22:03:51 +0100

    show both DI based construction or using a factory

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
+import { DependencyContainer } from 'tsyringe'
 
 import {
+  DITokens,
   EngineFactory,
   FixSession,
   IJsFixConfig,
@@ -12,16 +14,19 @@ import {
 import { MDClient } from './md-client'
 import { MDServer } from './md-server'
 import { MsgFact } from './msg-fact'
-import { MDTokens } from './md-tokens'
 import { MdController } from './md-controller'
+
 const commander = require('commander') // (normal include)
 const program = new commander.Command()
 program
   .option('-p, --port <number>', 'port for http controller', 3000)
+  .option('-i, --di', 'use DI based construction', false)
 
 program.parse()
-const port: number = program.opts().port
-console.log(`port: ${port}`)
+const opts = program.opts()
+const port: number = opts.port
+const useDI: boolean = opts.di
+console.log(`port: ${port}, DI: ${useDI ? 'using DI' : 'using factory'}`)
 
 class MySessionContainer extends SessionContainer {
   protected makeSessionFactory (description: ISessionDescription): ISessionMsgFactory {
@@ -33,33 +38,26 @@ class AppLauncher extends SessionLauncher {
   controller: MdController
 
   public constructor (
-    client = '../../data/session/test-initiator.json',
-    server = '../../data/session/test-acceptor.json') {
+    client: string,
+    server: string) {
     super(client, server)
     this.sessionContainer = new MySessionContainer()
     this.root = __dirname
   }
 
   // register a custom object with the DI container.
-  public makeController (c: IJsFixConfig, session: FixSession): MdController {
-    const sessionContainer = c.sessionContainer
-    sessionContainer.register<MdController>(MDTokens.MDController, {
-      useFactory: () => new MdController(c, session)
-    })
-    return sessionContainer.resolve<MdController>(MDTokens.MDController)
+
+  protected MakeServer (config: IJsFixConfig): FixSession {
+    const server = new MDServer(config)
+    this.controller = new MdController(config, server)
+    this.controller.start(port)
+    return server
   }
 
   stopController (): void {
     if (this.controller != null) {
       this.controller.stop()
     }
-  }
-
-  private makeSession (config: IJsFixConfig): FixSession {
-    const server = new MDServer(config)
-    this.controller = this.makeController(config, server)
-    this.controller.start(port)
-    return server
   }
 
   public launcher (): void {
@@ -71,18 +69,61 @@ class AppLauncher extends SessionLauncher {
       console.error(e)
     })
   }
+}
 
+class FactoryAppLauncher extends AppLauncher {
+  controller: MdController
+
+  public constructor (
+    client = '../../data/session/test-initiator.json',
+    server = '../../data/session/test-acceptor.json') {
+    super(client, server)
+  }
+
+  /* method 2: override this method for factory construction */
   protected override makeFactory (config: IJsFixConfig): EngineFactory {
     const isInitiator = this.isInitiator(config.description)
     const instance = this
-    const v: EngineFactory = {
+    return {
       makeSession: () => isInitiator
         ? new MDClient(config)
-        : instance.makeSession(config)
+        : instance.MakeServer(config)
     }
-    return v
   }
 }
 
-const l: AppLauncher = new AppLauncher()
+class DIAppLauncher extends AppLauncher {
+  controller: MdController
+
+  public constructor (
+    client = '../../data/session/test-initiator.json',
+    server = '../../data/session/test-acceptor.json') {
+    super(client, server)
+  }
+
+  protected asClient (sessionContainer: DependencyContainer): void {
+    sessionContainer.register<FixSession>(DITokens.FixSession, {
+      useClass: MDClient
+    })
+  }
+
+  protected asServer (sessionContainer: DependencyContainer): void {
+    const config = sessionContainer.resolve<IJsFixConfig>(DITokens.IJsFixConfig)
+    sessionContainer.register<FixSession>(DITokens.FixSession, {
+      useFactory: () => this.MakeServer(config)
+    })
+  }
+
+  protected override registerApplication (sessionContainer: DependencyContainer): void {
+    const config = sessionContainer.resolve<IJsFixConfig>(DITokens.IJsFixConfig)
+    const isInitiator = this.isInitiator(config.description)
+    if (isInitiator) {
+      this.asClient(sessionContainer)
+    } else {
+      this.asServer(sessionContainer)
+    }
+  }
+}
+
+const l: AppLauncher = useDI ? new DIAppLauncher() : new FactoryAppLauncher()
 l.launcher()
